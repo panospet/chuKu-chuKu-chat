5d61391dcd8436517d370af41ef697306bed4096
panospet <panospet@gmail.com>
2020-11-08 13:53:41 +0200

    small config changes

--- config/config.go
+++ config/config.go
@@ -21,14 +21,14 @@ type Config struct {
 
 func init() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatalln("No .env file found")
+		log.Print("No .env file found")
 	}
 }
 
 func NewConfig() (*Config, error) {
 	mode, ok := os.LookupEnv("MODE")
 	if !ok {
-		return nil, errors.New("MODE does not exist in .env file")
+		return nil, errors.New("MODE env variable does not exist")
 	}
 	if mode != DummyMode && mode != DbMode {
 		return nil, errors.New(fmt.Sprintf("MODE should have either value %s or %s", DummyMode, DbMode))
@@ -37,16 +37,16 @@ func NewConfig() (*Config, error) {
 	if mode == DbMode {
 		dsn, ok = os.LookupEnv("DSN")
 		if !ok {
-			return nil, errors.New("DSN does not exist in .env file")
+			return nil, errors.New("DSN env variable does not exist")
 		}
 	}
 	redis, ok := os.LookupEnv("REDIS_URL")
 	if !ok {
-		return nil, errors.New("REDIS_URL does not exist in .env file")
+		return nil, errors.New("REDIS_URL env variable does not exist")
 	}
 	nowPlayingUrl, ok := os.LookupEnv("NOW_PLAYING_URL")
 	if !ok {
-		return nil, errors.New("NOW_PLAYING_URL does not exist in .env file")
+		return nil, errors.New("NOW_PLAYING_URL env variable does not exist")
 	}
 	return &Config{
 		Mode:          mode,
--- docker-commands.sh
+++ docker-commands.sh
@@ -5,7 +5,7 @@ docker network create -d bridge --subnet=20.20.0.0/16 chuku
 docker run --rm --net chuku --ip 20.20.1.1 --name chukudb -d -v $(pwd)/testdata:/testdata -v $(pwd)/testdata/postgres:/var/lib/postgresql/data -e POSTGRES_PASSWORD=password -e POSTGRES_DB=chukudb -d -p 5432:5432 postgres:12.2-alpine
 
 # migrate
-CONFIG_FILE=./config/docker-config.yml  make migrate
+make migrate
 
 # redis
 docker run --rm --net chuku --ip 20.20.1.2 --name chuku-redis -d -p 6379:6379 redis
